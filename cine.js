96ecfe89ec345f635b9c2bb31daf59cfdfa739da
mmercedes011 <mninacolque@frba.utn.edu.ar>
2025-06-17 15:42:27 -0300

    Update cine.js

--- cine.js
+++ cine.js
@@ -1,22 +1,24 @@
 let asientos = [11, 15, 28, 44, 45, 70];
-
+let asiento_pedido = 50; // para que me corra en node ya le mando el asiento preterminado
 function cine(asientos, asiento_pedido) {
-    let disponible = false;
-
-    for (let i = 0; i <= 5; i++) {
-        if (asientos[i] === asiento_pedido) {
-            disponible = false;
-            break;
-        }
+  // Recorremos todos los asientos disponibles
+  for (let i = 0; i < asientos.length; i++) {
+    if (asientos[i] === asiento_pedido) {
+      // Si encontramos el asiento pedido en el array
+      console.log(
+        "Lo sentimos, el asiento " + asiento_pedido + " no está disponible."
+      );
+      return; // Salimos de la función para evitar seguir buscando
     }
-
-    if (disponible) {
-        console.log(" Felicitaciones, el asiento " + asiento_pedido+ " está disponible.");
-    } else {
-        console.log(" Lo sentimos, el asiento " + asiento_pedido + " no está disponible.");
-    }
+  }
+  // Si no se encontró el asiento en el ciclo, entonces está disponible
+  console.log(
+    "Felicitaciones, el asiento " + asiento_pedido + " está disponible."
+  );
 }
 
-let asiento_pedido = Number(prompt("Hola! ¿Qué asiento deseas comprar?"));
+// cuando uso el navegador puedo pedirle al usuario el asiento
+// let asiento_pedido = Number(prompt("¡Hola! ¿Qué asiento deseas comprar?"));
 
 cine(asientos, asiento_pedido);
+
